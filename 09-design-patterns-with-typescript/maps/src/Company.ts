78cc71cc66354b970d78e4bf4bba007a8e853c9d
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-06 05:25:40 +0600

    fixed the issues

---
+++ 09-design-patterns-with-typescript/maps/src/Company.ts
@@ -0,0 +1,30 @@
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+
+export class Company implements Mappable {
+    companyName: string;
+    catchPhase: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+    color: string = "red";
+
+    constructor() {
+        this.companyName = faker.company.companyName();
+        this.catchPhase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude()),
+        };
+    }
+
+    markerContent(): string {
+        return `
+            <div>
+                <h1>Company Name: ${this.companyName}</h1>
+                <h3>CatchPhase: ${this.catchPhase}</h3>
+            </div>
+        `;
+    }
+}
--- 09-design-patterns-with-typescript/maps/src/User.ts
+++ 09-design-patterns-with-typescript/maps/src/User.ts
@@ -1,11 +1,22 @@
-import faker from 'faker';
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+export class User implements Mappable {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+    color: string = "red";
 
-class User {
-  name: string;
-  location: {
-    lat: number;
-    lng: number;
-  };
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude()),
+        };
+    }
 
-  constructor() {}
+    markerContent(): string {
+        return `User Name: ${this.name}`;
+    }
 }
