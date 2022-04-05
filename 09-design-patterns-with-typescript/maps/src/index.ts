f4878e2864bf8980f2677b7cc6cc4e545684b966
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-05 19:32:15 +0600

    reduced code

--- 09-design-patterns-with-typescript/maps/src/index.ts
+++ 09-design-patterns-with-typescript/maps/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hi there!');
+import { User } from "./User";
+import { Company } from "./Company";
+
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
+customMap.addUserMarker(company);
