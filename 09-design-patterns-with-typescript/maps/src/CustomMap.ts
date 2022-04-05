3d1ff1178f89aef4328645cc934f7246026b7cc3
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-06 03:15:34 +0600

    added info on marker

--- 09-design-patterns-with-typescript/maps/src/CustomMap.ts
+++ 09-design-patterns-with-typescript/maps/src/CustomMap.ts
@@ -1,7 +1,3 @@
-// <reference types="@types/google.maps" />
-// import { User } from "./User";
-// import { Company } from "./Company";
-
 // Instructions to every other class
 // on how they can be an argument to 'addMarker'
 interface Mappable {
@@ -24,22 +20,20 @@ export class CustomMap {
     }
 
     addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng,
             },
         });
-    }
 
-    // addCompanyMarker(company: Company): void {
-    //   new google.maps.Marker({
-    //     map: this.googleMap,
-    //     position: {
-    //       lat: company.location.lat,
-    //       lng: company.location.lng
-    //     }
-    //   });
-    // }
+        marker.addListener("click", () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: "Hi there",
+            });
+
+            infoWindow.open(this.googleMap, marker);
+        });
+    }
 }
