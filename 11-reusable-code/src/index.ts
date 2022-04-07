a00b957c98f5dc128738ff70280e91ece0d8d2b9
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-08 03:24:38 +0600

    analyzied win matches

--- 11-reusable-code/src/index.ts
+++ 11-reusable-code/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello");
+import fs from "fs";
+
+const matches = fs
+    .readFileSync("football.csv", {
+        encoding: "utf-8",
+    })
+    .split("\n")
+    .map((row) => {
+        return row.split(",");
+    });
+
+let manUnited = 0;
+
+for (let match of matches) {
+    if (match[1] === "Man United" && match[5] === "H") {
+        manUnited++;
+    } else if (match[2] === "Man United" && match[5] === "A") {
+        manUnited++;
+    }
+}
+
+console.log(manUnited);
