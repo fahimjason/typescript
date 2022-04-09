20064c5d7c657f0eed531c281d8da3829f750bb7
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-09 15:58:41 +0600

    created User class

--- 13-web-framework/src/index.ts
+++ 13-web-framework/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hi');
+import { User } from './models/User';
+
+const user = new User({ name: 'Fahim', age: 27 });
+
+user.set({})
+console.log(user.get('name'));
+console.log(user.get('age'));
