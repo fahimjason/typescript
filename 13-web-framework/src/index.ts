f3b9efa3f5013f98fbb5b8fdbd44db5c7e03558c
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-09 16:23:53 +0600

    done event trigger

--- 13-web-framework/src/index.ts
+++ 13-web-framework/src/index.ts
@@ -2,6 +2,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Fahim', age: 27 });
 
-user.set({})
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+    console.log('change1');
+});
+user.on('change', () => {
+    console.log('change2');
+});
+user.on('ch', () => {
+    console.log('ch');
+});
+
+user.trigger('ch');
+console.log(user);
